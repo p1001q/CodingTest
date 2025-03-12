34ccc55acda0d46ea9f4c01627766217d06b6f38
p1001q <qrwrer123@gmail.com>
2025-03-12 13:51:36 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        if (num1==num2)
-            return 1;
-        else
-            return -1;
+        if (num1==num2) return 1;
+        else            return -1;
     }
 }
