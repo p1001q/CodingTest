e16eec3ac213173024314ba7e8a15058250edcb8
p1001q <qrwrer123@gmail.com>
2025-07-21 13:52:51 +0900

    [Bronze II] Title: OX퀴즈, Time: 140 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -5,23 +5,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 테스트 케이스 개수 입력
+        int n = Integer.parseInt(br.readLine()); //테스트 케이스 개수
 
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();  // OX 문자열 입력
-            int[] scores = new int[s.length()];
-            int sum = 0;
+            String s = br.readLine();  //OX 문자열 입력
+            int count = 0; //연속된 O의 개수
+            int sum = 0;   //점수 합계
 
             for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == 'O') {
-                    scores[j] = (j == 0) ? 1 : scores[j - 1] + 1; // 연속 O 점수
-                } else {
-                    scores[j] = 0; // X면 점수 0
+                if (s.charAt(j) == 'O') { //문자가 O면
+                    count++;      //연속 카운트 증가
+                    sum += count; //해당 점수 합산
+                } else {          //문자가 X면
+                    count = 0;    //연속 끊김
                 }
-                sum += scores[j]; // 합산
             }
-
-            bw.write(sum + "\n");
+            bw.write(sum + "\n"); //점수 출력
         }
 
         bw.flush();
