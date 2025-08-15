0050bea3a9421ba067e388e7c9f64bbf8066cf46
p1001q <qrwrer123@gmail.com>
2025-08-15 19:53:01 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "\\    /\\" << "\n";
+cout << " )  ( ')" << "\n";
+cout << "(  /  )" << "\n";
+cout << " \\(__)|" << "\n";
+} //백슬래시 출력하는 방법 익히는 거구나!
