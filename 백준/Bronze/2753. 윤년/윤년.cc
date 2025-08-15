1a5853f919e0df53e56d2aae7cb25ec082f27ab2
p1001q <qrwrer123@gmail.com>
2025-08-15 19:09:26 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -5,10 +5,7 @@ int main() {
     int a;
     cin >> a;
 
-    if (a % 4 == 0 && a % 100 != 0) {
-        cout << 1;
-    }
-    else if (a % 400 == 0) {
+    if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)) {
         cout << 1;
     }
     else {
