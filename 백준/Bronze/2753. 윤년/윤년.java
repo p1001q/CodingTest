e0a4f6dc7a8be897452d951e04f9d9a7f7f751d7
p1001q <qrwrer123@gmail.com>
2025-08-15 19:31:25 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if ((a%4==0 && a%100!=0) || (a%400==0)) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+} //자바로 진짜 어케 푸는 거임...
